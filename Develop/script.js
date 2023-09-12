a8a9bc53243c54af1378ee7adeff5072d0e144f7
NoFox <nfox598625@gmail.com>
2023-09-11 20:11:53 -0400

    added prompt to see how long someone would like the password and added comfirms to find out if they want different characters. Still working on the incorrect respones

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,58 @@
 // Assignment code here
+var lettersl = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var lettersu = ["A","B","C","D","E", "F","G", "H","I","J","K","L","M","N","O","P","Q","R","S","T","U", "V","W","X","Y","Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["@","#","$","%","^","&","*","()","_","-","=",">","<","/","~",];
 
+var length = 8;
+var char = [];
+
+function settings() {
+    char = [];
+    length =  parseInt(prompt('How long would you like your password pick 8 - 128 characters.'));
+
+    if (isNaN(length) || length < 8 || length > 128){
+      alert('Pick bewteen 8 - 128')
+      return false;
+    } 
+
+    if (confirm('Would you like any lowercase letters?')){
+      char = char.concat(lettersl); 
+     }
+    if (confirm('Would you like any uppercase letters?')){
+    char = char.concat(lettersu);
+    }
+   if (confirm('Would you like any numbers?')){
+    char = char.concat(numbers); 
+    }
+    if (confirm('Would you like any symbols?')){
+    char = char.concat(symbols); 
+    }
+    return true;
+}
+
+
+
+function generatePassword() {
+
+  var password = '';
+  for(var i = 0; i < length; i++){
+    var index = Math.floor(Math.random() * char.length);
+    password = password + char[index];
+}
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  settings();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
